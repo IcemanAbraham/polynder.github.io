b709a2df7e28656acef07d09e45b13c172c4be52
IcemanAbraham <65927150+IcemanAbraham@users.noreply.github.com>
2020-05-27 18:51:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ try {
   $bdd = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
 } catch (PDOException $e) {
   echo "Erreur!: " . $e->getMessage() . "<br/>";
-  die();
+  
 }
 
 $id=(isset($_GET['idfb']))?(int) $_GET['idfb']:0;
